4f43b8d61ea0f94792526ac29bb7e1c5a3ec8343
anwar <mahedianwar@gmail.com>
2023-08-30 07:34:10 +0600

    feat: setup template

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// Change your rules accordingly to your coding style preferencies.
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+    trailingComma: 'es5',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+};
